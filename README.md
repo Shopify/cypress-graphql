6fec726015ff5e6db8757ce2f56512b32f796734
Ryan Keller <ryan.keller@shopify.com>
2022-03-02 14:40:09 -0600

    fix: more boilerplate and testing

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # `@shopify/cypress-graphql`
 
-[![Build Status](https://github.com/Shopify/quilt/workflows/Node-CI/badge.svg?branch=main)](https://github.com/Shopify/quilt/actions?query=workflow%3ANode-CI)
-[![Build Status](https://github.com/Shopify/quilt/workflows/Ruby-CI/badge.svg?branch=main)](https://github.com/Shopify/quilt/actions?query=workflow%3ARuby-CI)
+[![Build Status](https://github.com/Shopify/cypress-graphql/workflows/CI/badge.svg?branch=main)](https://github.com/Shopify/cypress-graphql/actions?query=workflow%3ACI)
 [![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE.md) [![npm version](https://badge.fury.io/js/%40shopify%2Fcypress-graphql.svg)](https://badge.fury.io/js/%40shopify%2Fcypress-graphql.svg) [![npm bundle size (minified + gzip)](https://img.shields.io/bundlephobia/minzip/@shopify/cypress-graphql.svg)](https://img.shields.io/bundlephobia/minzip/@shopify/cypress-graphql.svg)
 
 Cypress commands for intercepting and testing graphql endpoints.
@@ -14,4 +13,8 @@ $ yarn add @shopify/cypress-graphql
 
 ## Usage
 
-WIP
+```
+cy.interceptGql([
+  ['StoreQuery', { statusCode: 200, body: { data: MockStoreQueryData } }],
+])
+```
--- cypress/integration/interceptGql.spec.ts
+++ cypress/integration/interceptGql.spec.ts
@@ -1,4 +1,9 @@
 context('cy.interceptGql', () => {
+  // Set up a mock graphql endpoint using a normal cypress route interceptor
+  beforeEach(() => {
+    cy.intercept('POST', '**/api/graphql', { data: 'Default response body' }).as('MockAPI')
+  })
+
   describe('registering the command', () => {
     it('should register the command automatically', () => {
       cy.wrap(cy.interceptGql)
@@ -7,21 +12,44 @@ context('cy.interceptGql', () => {
     });
   });
 
-  describe('usage', () => {
+  describe('spying on requests (no modifications)', () => {
     beforeEach(() => {
       // Intercept requests to /api/graphql
       cy.interceptGql('**/api/graphql', ['TestQuery', 'TestMutation'])
-        // Visit the example app, which is a page that will trigger a query and mutation
-        .visit('cypress/pages/index.html');
+      // Visit the example app, which is a page that will trigger a query and mutation
+      cy.visit('cypress/pages/index.html');
     });
 
-    it('should be able to intercept graphql calls in general', () => {
+    it('should be able to spy on graphql calls in general', () => {
       cy.wait('@GraphQL');
     });
 
-    it('should be able to intercept a specific operation', () => {
+    it('should be able to spy on a specific operation', () => {
       cy.wait('@TestQuery')
         .wait('@TestMutation');
+    });
+
+    it('should be able to stub a specific operation', () => {
+      cy.wait('@TestQuery')
+        .wait('@TestMutation');
+    });
+  });
+
+  describe('stubbing requests (providing a response)', () => {
+    beforeEach(() => {
+      // Stub requests for this endpoint
+      cy.interceptGql('**/api/graphql', [
+        ['TestQuery', { statusCode: 200, body: { data: 'Intercepted Query!' } }],
+        ['TestMutation', { statusCode: 200, body: { data: 'Intercepted Mutation!' } }]
+      ])
+      cy.visit('cypress/pages/index.html');
+    });
+
+    it('requests will receive a stub response if provided', () => {
+      cy.wait('@TestQuery')
+        .its('response.body.data').should('eq', 'Intercepted Query!');
+      cy.wait('@TestMutation')
+        .its('response.body.data').should('eq', 'Intercepted Mutation!');
     });
   });
 });
